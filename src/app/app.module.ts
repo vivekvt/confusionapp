672f06355a9a1b3fbaf2c488f20797fd15d1c30f
VTVIVEK <vivek78643@gmail.com>
2020-04-13 13:26:00 +0530

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms'; 
-
+import { ReactiveFormsModule } from '@angular/forms';
 import 'hammerjs';
 
 
@@ -50,7 +52,8 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     BrowserAnimationsModule, 
     FlexLayoutModule,
-    FormsModule, 
+    FormsModule,
+    ReactiveFormsModule, 
     MatToolbarModule, 
     MatListModule, 
     MatGridListModule, 
@@ -60,6 +63,8 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule
 
   ],
   providers: [DishService, PromotionService, LeaderService],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
